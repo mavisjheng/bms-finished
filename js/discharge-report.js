09d3ba0de7de1714c61b924d49f123c217d110f2
mavisjheng <erjhen@gmail.com>
2015-05-06 10:01:52 +0800

    Finish module/discharge-report page

--- js/discharge-report.js
+++ js/discharge-report.js
@@ -1,11 +1,50 @@
 $(document).ready(function() {
-    
-    // update cell-history-morris-chart upon the type selection changes
-    // morris chart
+    //dataTable
+    $('#finish-discharge-table').dataTable({
+        lengthMenu: [ [5, 10, 20, 30, 50, -1], [5, 10, 20, 30, 50, "所有"] ],
+        length: false,
+        ordering: false,
+        paging: true,
+        info: true,
+        filter: true,
+        language: {
+            lengthMenu: "显示 _MENU_ 项结果",
+            zeroRecords: "没有匹配结果",
+            info: "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+            infoEmpty: "显示第 0 至 0 项结果，共 0 项",
+            infoFiltered: "(由 _MAX_ 项结果过滤)",
+            search: "搜索：",
+            paginate: {
+                previous: "上页",
+                next: "下页",
+                first: "首页",
+                last: "末页"
+            }
+        }
+    });
+
     var initData = prepareDemoCellData();
-    
-    var graph1 = new Morris.Line({
-        element: 'voltage-chart',
+    var reportSingleVoltage = new Morris.Line({
+        element: 'report-single-voltage',
+        data: [
+            { y: '2006', a: 100, b: 90 },
+            { y: '2007', a: 75,  b: 65 },
+            { y: '2008', a: 50,  b: 40 },
+            { y: '2009', a: 75,  b: 65 },
+            { y: '2010', a: 50,  b: 40 },
+            { y: '2011', a: 75,  b: 65 },
+            { y: '2012', a: 100, b: 90 }
+        ],
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        labels: ['Series A', 'Series B'],
+        ymin: 0,
+        ymax: 200,
+        lineColors: ['purple']
+    });
+
+    var reportTotalVoltage = new Morris.Line({
+        element: 'report-total-voltage',
         data: initData,
         xkey: 'year',
         ykeys: ['value'],
@@ -13,48 +52,6 @@ $(document).ready(function() {
         ymin: 0,
         ymax: 200,
         lineColors: ['purple']
-    });
-
-    var graph2 = new Morris.Line({
-        element: 'current-chart',
-        data: initData,
-        xkey: 'year',
-        ykeys: ['value'],
-        labels: ['平均'],
-        ymin: 0,
-        ymax: 200,
-        lineColors: ['purple']
-    });
-
-    var graph3 = new Morris.Bar({
-        element: 'capacity-chart',
-        data: [
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-        ],
-        xkey: 'x',
-        ykeys: ['y'],
-        labels: ['即时电压']
-    });
-
-    var graph4 = new Morris.Bar({
-        element: 'current',
-        data: [
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-        ],
-        xkey: 'x',
-        ykeys: ['y'],
-        labels: ['即时电压'],
-        resize: true
     });
 
     function prepareDemoCellData() {
@@ -74,4 +71,10 @@ $(document).ready(function() {
         }
         return data;
     }
+
+    // redraw the morris charts which are at hidden tabs
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        reportSingleVoltage.redraw();
+        reportTotalVoltage.redraw();
+    });
 });
--- js/module.js
+++ js/module.js
@@ -17,32 +17,10 @@ $(document).ready(function() {
         $(this).next().focus();
     });
 
-    // pie chart
-    $('.easy-pie-chart.percentage').each(function() {
-        var $box = $(this).closest('.infobox');
-        $(this).easyPieChart({
-            barColor: function colorChange(percentage) {
-                if (percentage < 50) {
-                    return '#d4301d';
-                } else if (percentage > 49 && percentage < 80){
-                    return '#f6a509';
-                } else {
-                    return '#69aa46';
-                }
-            },
-            trackColor: '#E2E2E2',
-            lineWidth: 15,
-            lineCap: 'butt',
-            scaleColor: false,
-            size: 120,
-        });
-    })
-
-    // update cell-history-morris-chart upon the type selection changes
     // morris chart
     var initData = prepareDemoCellData();
     
-    var graph1 = new Morris.Line({
+    var voltage = new Morris.Line({
         element: 'voltage-chart',
         data: initData,
         xkey: 'year',
@@ -53,7 +31,7 @@ $(document).ready(function() {
         lineColors: ['purple']
     });
 
-    var graph2 = new Morris.Line({
+    var current = new Morris.Line({
         element: 'current-chart',
         data: initData,
         xkey: 'year',
@@ -64,33 +42,156 @@ $(document).ready(function() {
         lineColors: ['purple']
     });
 
-    var graph3 = new Morris.Bar({
-        element: 'capacity-chart',
+    var historyVoltage = new Morris.Line({
+        element: 'history-voltage',
+        data: initData,
+        xkey: 'year',
+        ykeys: ['value'],
+        labels: ['平均'],
+        ymin: 0,
+        ymax: 200,
+        lineColors: ['purple']
+    });
+
+    var historyCurrent = new Morris.Line({
+        element: 'history-current',
+        data: initData,
+        xkey: 'year',
+        ykeys: ['value'],
+        labels: ['平均'],
+        ymin: 0,
+        ymax: 200,
+        lineColors: ['purple']
+    });
+
+    var dischargeSingleVoltage = new Morris.Line({
+        element: 'discharge-single-voltage',
         data: [
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
+            { y: '2006', a: 100, b: 90 },
+            { y: '2007', a: 75,  b: 65 },
+            { y: '2008', a: 50,  b: 40 },
+            { y: '2009', a: 75,  b: 65 },
+            { y: '2010', a: 50,  b: 40 },
+            { y: '2011', a: 75,  b: 65 },
+            { y: '2012', a: 100, b: 90 }
         ],
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        labels: ['Series A', 'Series B'],
+        ymin: 0,
+        ymax: 200,
+        lineColors: ['purple']
+    });
+
+    var dischargeTotalVoltage = new Morris.Line({
+        element: 'discharge-total-voltage',
+        data: initData,
+        xkey: 'year',
+        ykeys: ['value'],
+        labels: ['平均'],
+        ymin: 0,
+        ymax: 200,
+        lineColors: ['purple']
+    });
+
+    var currentVoltage = new Morris.Bar({
+        element: 'current-voltage',
+        data: [
+            { x: '电池1', y: 75 },
+            { x: '电池2', y: 75 },
+            { x: '电池3', y: 75 },
+            { x: '电池4', y: 75 },
+            { x: '电池5', y: 75 },
+            { x: '电池6', y: 75 },
+            { x: '电池7', y: 75 },
+            { x: '电池8', y: 75 },
+            { x: '电池9', y: 75 },
+            { x: '电池10', y: 75 },
+            { x: '电池11', y: 75 },
+            { x: '电池12', y: 75 },
+            { x: '电池13', y: 75 },
+            { x: '电池14', y: 75 },
+            { x: '电池15', y: 75 },
+            { x: '电池16', y: 75 },
+            { x: '电池17', y: 75 },
+            { x: '电池18', y: 75 },
+            { x: '电池19', y: 75 },
+            { x: '电池20', y: 75 },
+            { x: '电池21', y: 75 },
+            { x: '电池22', y: 75 },
+            { x: '电池23', y: 75 },
+            { x: '电池24', y: 75 },
+        ],
+        xkey: 'x',
         ykeys: ['y'],
         labels: ['即时电压']
     });
 
-    var graph4 = new Morris.Bar({
-        element: 'current',
+    var currentCapacity = new Morris.Bar({
+        element: 'current-capacity',
         data: [
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
-            { x: '1', y: 75 },
+            { x: '电池1', y: 75 },
+            { x: '电池2', y: 75 },
+            { x: '电池3', y: 75 },
+            { x: '电池4', y: 75 },
+            { x: '电池5', y: 75 },
+            { x: '电池6', y: 75 },
+            { x: '电池7', y: 75 },
+            { x: '电池8', y: 75 },
+            { x: '电池9', y: 75 },
+            { x: '电池10', y: 75 },
+            { x: '电池11', y: 75 },
+            { x: '电池12', y: 75 },
+            { x: '电池13', y: 75 },
+            { x: '电池14', y: 75 },
+            { x: '电池15', y: 75 },
+            { x: '电池16', y: 75 },
+            { x: '电池17', y: 75 },
+            { x: '电池18', y: 75 },
+            { x: '电池19', y: 75 },
+            { x: '电池20', y: 75 },
+            { x: '电池21', y: 75 },
+            { x: '电池22', y: 75 },
+            { x: '电池23', y: 75 },
+            { x: '电池24', y: 75 },
         ],
         xkey: 'x',
         ykeys: ['y'],
-        labels: ['即时电压'],
+        labels: ['目前容量'],
+        resize: true
+    });
+
+    var historyCapacity = new Morris.Bar({
+        element: 'module-history-capacity',
+        data: [
+            { x: '电池1', y: 75 },
+            { x: '电池2', y: 75 },
+            { x: '电池3', y: 75 },
+            { x: '电池4', y: 75 },
+            { x: '电池5', y: 75 },
+            { x: '电池6', y: 75 },
+            { x: '电池7', y: 75 },
+            { x: '电池8', y: 75 },
+            { x: '电池9', y: 75 },
+            { x: '电池10', y: 75 },
+            { x: '电池11', y: 75 },
+            { x: '电池12', y: 75 },
+            { x: '电池13', y: 75 },
+            { x: '电池14', y: 75 },
+            { x: '电池15', y: 75 },
+            { x: '电池16', y: 75 },
+            { x: '电池17', y: 75 },
+            { x: '电池18', y: 75 },
+            { x: '电池19', y: 75 },
+            { x: '电池20', y: 75 },
+            { x: '电池21', y: 75 },
+            { x: '电池22', y: 75 },
+            { x: '电池23', y: 75 },
+            { x: '电池24', y: 75 },
+        ],
+        xkey: 'x',
+        ykeys: ['y'],
+        labels: ['历史容量'],
         resize: true
     });
 
@@ -112,13 +213,37 @@ $(document).ready(function() {
         return data;
     }
 
-    $("#module-chart-form").submit(function(event) {
-        event.preventDefault();
-        var chartType = $("#module-chart-type option:selected").val();
-        if (chartType) {
-            var cellData = prepareDemoCellData();
-            graph.setData(cellData);
+    //dataTable
+    $('#module-cell-info-table, #module-alarm-log-table').dataTable({
+        lengthMenu: [ [10, 20, 30, 50, -1], [10, 20, 30, 50, "所有"] ],
+        length: false,
+        ordering: false,
+        paging: true,
+        info: true,
+        filter: true,
+        language: {
+            lengthMenu: "显示 _MENU_ 项结果",
+            zeroRecords: "没有匹配结果",
+            info: "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+            infoEmpty: "显示第 0 至 0 项结果，共 0 项",
+            infoFiltered: "(由 _MAX_ 项结果过滤)",
+            search: "搜索：",
+            paginate: {
+                previous: "上页",
+                next: "下页",
+                first: "首页",
+                last: "末页"
+            }
         }
     });
 
+    // redraw the morris charts which are at hidden tabs
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        currentCapacity.redraw();
+        historyCapacity.redraw();
+        historyVoltage.redraw();
+        historyCurrent.redraw();
+        dischargeSingleVoltage.redraw();
+        dischargeTotalVoltage.redraw();
+    });
 });
